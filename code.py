6407c8f83378e1bc0969fc3697816ea01279f657
KHIANWEI <60164497+KHIANWEI@users.noreply.github.com>
2020-02-26 15:37:32 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,5 @@
 def add(a,b):
     return a+b
 
-def no_op(a):
-    '''
-        Write a meaningful function here, unlike the one above
-
-        Then, add a test case for this function in tests/test.py'''
+def myFunc(a):
     return a*2;
